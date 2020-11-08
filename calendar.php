d444d312f820f5c9e59f166f4a91edd8aede8787
apple <a8510110981@gmail.com>
2020-11-08 14:15:29 +0800

    年月份純功能

---
+++ calendar.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+    
+    <style>
+    table{
+        width:750px;
+        margin:auto;
+        border:1px solid #ccc;
+    }
+    table td{
+        border:1px solid #ccc;
+        text-align:center;
+        padding:10px;
+    }
+    table td:hover{
+        background:#ccc;
+    }
+    </style>
+</head>
+<body>
+
+    
+<?php
+
+
+
+    if(isset($_GET['month']) || isset($_GET['year'])){
+        $toMonth=$_GET['month'];
+        $toYear=$_GET['year'];
+    }else {
+        $toYear=date("Y");
+        $toMonth=date("m");
+
+    }
+    
+    if($toMonth >= 12){
+        $nextMonth=1;
+        $nextYear=$toYear+1;
+
+    }
+    else{
+        $nextMonth=$toMonth+1;
+        $nextYear=$toYear;
+
+    }
+    
+
+    if($toMonth <=1){
+        $lastMonth=12;
+        $lastYear=$toYear-1;
+
+    }else {
+        $lastMonth=$toMonth-1;
+        $lastYear=$toYear;
+
+    }
+        // echo "這年 $toYear";
+        // echo "這個月測試 $toMonth";
+    $allDaylast=date("t");
+    $firstDay=strtotime($toYear."-".$toMonth."-".'1');
+    $allDay =date("t",$firstDay);
+    $startDay=date("w",$firstDay);
+
+
+?>
+    <div align="center" valign="center" >
+    <h1>萬年曆</h1>
+    </div>
+    <div align="center" valign="center" >
+        <h6><?=$toYear;?>年<?=$toMonth;?>月</h6>
+    </div>
+    <div class='d-flex justify-content-between  m-auto' style="width:750px" >
+        <a href="calendar.php?month=<?=$lastMonth;?>&year=<?=$lastYear;?>">Last</a>
+        <a href="calendar.php?month=<?=$nextMonth;?>&year=<?=$nextYear;?>">Next</a>
+    </div>
+
+
+<table>
+    <tr>
+        <td>日</td>
+        <td>一</td>
+        <td>二</td>
+        <td>三</td>
+        <td>四</td>
+        <td>五</td>
+        <td>六</td>
+    </tr>
+<?php
+
+    
+    if($startDay + $allDay <=28){
+        $week =4;
+    }else if($startDay + $allDay >28 && $startDay + $allDay <=35 ){
+        $week =5;
+    }else if($startDay + $allDay >35 && $startDay + $allDay <=42 ){
+        $week =6;
+    }
+
+    for($i=0; $i<$week ;$i++){
+
+        echo "<tr>";
+        for($j=0;$j<7;$j++){
+            echo "<td>";
+
+            if($i==0 && $j<$startDay){
+                
+            }else if(($i*7) + ($j+1)-$startDay>$allDay){
+                echo "&nbsp";
+            }
+            else{
+
+                echo (($i*7) + ($j+1))-$startDay;
+            }
+
+            echo "</td>";
+
+        }
+        echo "</tr>";
+    }
+
+?>
+</table>
+
+</body>
+</html>
